8ab99e7875eeaede95d321562329a2dfe78c1ca6
Sven Klemmer <svenklemmer@t-online.de>
2019-03-28 22:12:48 +0100

    construct some sketchy docker stuff

--- k8s/test.sh
+++ k8s/test.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-tools=( "kubectl version" "helm version" "kubens" "kubectx" )
+tools=(
+    "kubectl version"
+    "helm version -c"
+    "kubens -h"
+    "kubectx -h"
+    "helmsman -v"
+    )
 
 for t in $(tools); do
     docker run -it ${TOOL}:latest ${t}
